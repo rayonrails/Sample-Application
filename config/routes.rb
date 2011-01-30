60fcb04abd6952c46bc9ae42a725ee871727152e
Ray Rogers <ray@rayonrails.com>
2011-01-30 11:47:11 -0500

    Removed default Rails Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
+  root :to => "pages#home"
 end
