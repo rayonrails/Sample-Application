bdde7e70cd2b28f4a92ede94e487b28205f02555
Ray Rogers <ray@rayonrails.com>
2011-02-02 19:26:48 -0500

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -4,10 +4,12 @@ gem 'rails', '3.0.3'
 gem 'mysql2'
 
 group :development do
-	gem 'rspec-rails','2.0.1'
+	gem 'rspec-rails','2.4.0'
+	gem 'annotate-models', '1.0.4'
 end
 
 group :test do
-	gem 'rspec', '2.0.1'
+	gem 'rspec', '2.4.0'
 	gem 'webrat', '0.7.1'
+	gem 'annotate-models','1.0.4'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 SampleApp::Application.routes.draw do
-
-
+  resources :users
+  
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
-  get "users/new"
   get "pages/home"
   get "pages/contact"
   get "pages/about"
