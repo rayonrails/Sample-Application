e0a5e691df8919e74fbbf340b6ad38857a08661b
Ray Rogers <ray@rayonrails.com>
2011-02-05 12:10:44 -0500

    layout fixes for user signup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,5 +19,22 @@ describe "Users" do
        end.should_not change(User, :count)
      end
     end
+  
+    describe "success" do
+      
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button
+          response.should have_selector("div.flash.sucess",
+                                         :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+      end
+    end
   end
 end
